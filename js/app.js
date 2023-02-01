216ca750facd318db28ee754fab6cfb30ff05812
Kyle Freemantle <kcfreemantle@gmail.com>
2023-01-31 23:30:37 -0800

    trying to write to html table

--- js/app.js
+++ js/app.js
@@ -21,18 +21,18 @@ function calculateSalesAverages(avg, min, max){   // we set parameters for avera
 }
 
 
-// STOREFRONT OBJECTS
 
+// Constructor function StoreLocation.  Builds an object for each store, requiring the name, min/max customer and average sales data provided.
 
-// Seattle
-
-let storeSeattle = {
-  name: 'Seattle',
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgSales: 6.3,
-  hourlySalesArray: [],
-  renderList: function () {
+function StoreLocation(name, minCustomer, maxCustomer, avgSales) {
+  this.name = name,
+  this.minCustomer = minCustomer,
+  this.maxCustomer = maxCustomer,
+  this.avgSales = avgSales,
+  this.hourlySalesArray = [],
+  this.avgSalesEst = function(){
+    this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);}
+  this.renderList = function () {
     this.avgSalesEst();
     let ul = document.createElement('ul');
     for (let i = 0; i < hours.length; i++) {
@@ -42,136 +42,203 @@ let storeSeattle = {
       ul.appendChild(li);
     }
     salesContainer.appendChild(ul);
-  },
-  
-  avgSalesEst: function(){
-    this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
   }
+};
+
+
+// StoreLocation Prototype methods, trying to render our data into a HTML table
+
+StoreLocation.prototype.render = function() {
+  const containerElement = document.getElementById('salesList');
+  // this is the window to the DOM.  salesList is the ID tied to the table element.  When we use the createElement function later it's going to append the contents we create only to that element.
+  const tableElement = document.createElement('table');
+  containerElement.appendChild(tableElement);  // this creates the table element inside the salesList id'd section of the document, which is <section> at the time of this writing.
+
+  const tfElement = document.createElement('tfoot');
+  const tdElement = document.createElement('td');
   
+  
+  // so we now we have a table to write to.  We need to add a header row for the times of day.
+  const trElement = document.createElement('tr');
+  tableElement.appendChild(trElement);
+  
+  // Now that we have a table row for our headers and we want to add header cells to the table row element, we want to add all of our times from the hours array.  So we'll use a loop.
+  
+  for (let i = 0; i < this.hours.length; i++) {
+    const thElement = document.createElement('th');  
+    trElement.appendChild(thElement);
+    thElement.textContent = this.hours[i];
+
+
+  } 
+  // hours.unshift('~');  // this is an attempt to nudge the hours over by one in the table.
+
+  // // now we need to add rows and data for each store
+
+  
+
+
+
+
+
 }
 
-console.log(storeSeattle);
-storeSeattle.renderList();
+let seattle = new StoreLocation(
+  'Seattle',
+  23, 65, 6.3);
 
-// Tokyo
+  seattle.renderList();
+// // STOREFRONT OBJECTS
 
-let storeTokyo = {
-  name: 'Tokyo',
-  minCustomer: 2,
-  maxCustomer: 24,
-  avgSales: 1.2,
-  hourlySalesArray: [],
-  
-  renderList: function () {
-    this.avgSalesEst();
-    let ul = document.createElement('ul');
-    for (let i = 0; i < hours.length; i++) {
-      let li = document.createElement('li');
+
+// // Seattle
+
+// let storeSeattle = {
+//   name: 'Seattle',
+//   minCustomer: 23,
+//   maxCustomer: 65,
+//   avgSales: 6.3,
+//   hourlySalesArray: [],
+//   renderList: function () {
+//     this.avgSalesEst();
+//     let ul = document.createElement('ul');
+//     for (let i = 0; i < hours.length; i++) {
+//       let li = document.createElement('li');
       
-      li.textContent = `${hours[i]}: ${this.hourlySalesArray[i]}`;
-      ul.appendChild(li);
-    }
-    salesContainer.appendChild(ul);
-  },
+//       li.textContent = `${hours[i]}: ${this.hourlySalesArray[i]}`;
+//       ul.appendChild(li);
+//     }
+//     salesContainer.appendChild(ul);
+//   },
   
-  avgSalesEst: function(){
-    this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
-  }
+//   avgSalesEst: function(){
+//     this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
+//   }
   
-}
+// }
 
-console.log(storeTokyo);
-storeTokyo.renderList();
+// console.log(storeSeattle);
+// storeSeattle.renderList();
 
+// // Tokyo
 
-// Dubai
-
-let storeDubai = {
-  name: 'Dubai',
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgSales: 3.7,
-  hourlySalesArray: [],
+// let storeTokyo = {
+//   name: 'Tokyo',
+//   minCustomer: 2,
+//   maxCustomer: 24,
+//   avgSales: 1.2,
+//   hourlySalesArray: [],
   
-  renderList: function () {
-    this.avgSalesEst();
-    let ul = document.createElement('ul');
-    for (let i = 0; i < hours.length; i++) {
-      let li = document.createElement('li');
+//   renderList: function () {
+//     this.avgSalesEst();
+//     let ul = document.createElement('ul');
+//     for (let i = 0; i < hours.length; i++) {
+//       let li = document.createElement('li');
       
-      li.textContent = `${hours[i]}: ${this.hourlySalesArray[i]}`;
-      ul.appendChild(li);
-    }
-    salesContainer.appendChild(ul);
-  },
+//       li.textContent = `${hours[i]}: ${this.hourlySalesArray[i]}`;
+//       ul.appendChild(li);
+//     }
+//     salesContainer.appendChild(ul);
+//   },
   
-  avgSalesEst: function(){
-    this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
-  }
+//   avgSalesEst: function(){
+//     this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
+//   }
   
-}
+// }
 
-console.log(storeDubai);
-storeDubai.renderList();
+// console.log(storeTokyo);
+// storeTokyo.renderList();
 
 
-// Paris
+// // Dubai
 
-let storeParis = {
-  name: 'Paris',
-  minCustomer: 20,
-  maxCustomer: 38,
-  avgSales: 2.3,
-  hourlySalesArray: [],
+// let storeDubai = {
+//   name: 'Dubai',
+//   minCustomer: 11,
+//   maxCustomer: 38,
+//   avgSales: 3.7,
+//   hourlySalesArray: [],
   
-  renderList: function () {
-    this.avgSalesEst();
-    let ul = document.createElement('ul');
-    for (let i = 0; i < hours.length; i++) {
-      let li = document.createElement('li');
+//   renderList: function () {
+//     this.avgSalesEst();
+//     let ul = document.createElement('ul');
+//     for (let i = 0; i < hours.length; i++) {
+//       let li = document.createElement('li');
       
-      li.textContent = `${hours[i]}: ${this.hourlySalesArray[i]}`;
-      ul.appendChild(li);
-    }
-    salesContainer.appendChild(ul);
-  },
+//       li.textContent = `${hours[i]}: ${this.hourlySalesArray[i]}`;
+//       ul.appendChild(li);
+//     }
+//     salesContainer.appendChild(ul);
+//   },
   
-  avgSalesEst: function(){
-    this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
-  }
+//   avgSalesEst: function(){
+//     this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
+//   }
   
-}
+// }
 
-console.log(storeParis);
-storeParis.renderList();
+// console.log(storeDubai);
+// storeDubai.renderList();
 
 
-// Lima
+// // Paris
 
-let storeLima = {
-  name: 'Lima',
-  minCustomer: 2,
-  maxCustomer: 16,
-  avgSales: 4.6,
-  hourlySalesArray: [],
+// let storeParis = {
+//   name: 'Paris',
+//   minCustomer: 20,
+//   maxCustomer: 38,
+//   avgSales: 2.3,
+//   hourlySalesArray: [],
   
-  renderList: function () {
-    this.avgSalesEst();
-    let ul = document.createElement('ul');
-    for (let i = 0; i < hours.length; i++) {
-      let li = document.createElement('li');
+//   renderList: function () {
+//     this.avgSalesEst();
+//     let ul = document.createElement('ul');
+//     for (let i = 0; i < hours.length; i++) {
+//       let li = document.createElement('li');
       
-      li.textContent = `${hours[i]}: ${this.hourlySalesArray[i]}`;
-      ul.appendChild(li);
-    }
-    salesContainer.appendChild(ul);
-  },
+//       li.textContent = `${hours[i]}: ${this.hourlySalesArray[i]}`;
+//       ul.appendChild(li);
+//     }
+//     salesContainer.appendChild(ul);
+//   },
   
-  avgSalesEst: function(){
-    this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
-  }
+//   avgSalesEst: function(){
+//     this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
+//   }
   
-}
+// }
 
-console.log(storeLima);
-storeLima.renderList();
+// console.log(storeParis);
+// storeParis.renderList();
+
+
+// // Lima
+
+// let storeLima = {
+//   name: 'Lima',
+//   minCustomer: 2,
+//   maxCustomer: 16,
+//   avgSales: 4.6,
+//   hourlySalesArray: [],
+  
+//   renderList: function () {
+//     this.avgSalesEst();
+//     let ul = document.createElement('ul');
+//     for (let i = 0; i < hours.length; i++) {
+//       let li = document.createElement('li');
+      
+//       li.textContent = `${hours[i]}: ${this.hourlySalesArray[i]}`;
+//       ul.appendChild(li);
+//     }
+//     salesContainer.appendChild(ul);
+//   },
+  
+//   avgSalesEst: function(){
+//     this.hourlySalesArray = calculateSalesAverages(this.avgSales, this.minCustomer, this.maxCustomer);
+//   }
+  
+// }
+
+// console.log(storeLima);
+// storeLima.renderList();
